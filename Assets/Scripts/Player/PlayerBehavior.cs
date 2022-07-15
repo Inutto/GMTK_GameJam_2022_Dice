8354f1c90c398b2dce8ae4e08efe47f2cd2d4135
Quin7et <quin7et@gmail.com>
2022-07-16 06:31:13 +0800

    Timer Class

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -10,6 +10,7 @@ public class PlayerBehavior : GridObject
 
     private void Update()
     {
+
         
     }
 
--- Assets/Scripts/Tools/TimerF.cs
+++ Assets/Scripts/Tools/TimerF.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TimerF : MonoBehaviour
+    
+public class TimerF
 {
-    // Start is called before the first frame update
-    void Start()
+    float maxTime;
+    float remainingTime;
+
+    public bool isTicking { get; private set; }
+
+    public bool isFinished { get { return remainingTime < 0; } }
+
+    public TimerF(float time)
     {
-        
+        maxTime = time;
+        remainingTime = time;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Begin()
     {
-        
+        isTicking = true;
+    }
+
+    public void Pause()
+    {
+        isTicking = false;
+    }
+
+    public void ResetAndPause()
+    {
+        remainingTime = maxTime;
+        isTicking = false;
+    }
+
+    public void ResetAndBegin()
+    {
+        remainingTime = maxTime;
+        isTicking = true;
+    }
+
+    public void ChangeMaxTime(float newmax)
+    {
+        maxTime = newmax;
+        remainingTime = newmax;
+        isTicking = false;
+    }
+
+    // Start is called before the first frame update
+    public void FixedTick()
+    {
+        if (!isTicking) return;
+        if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            isTicking = false;
+        }
+
+        remainingTime -= Time.fixedDeltaTime;
+    }
+
+    public void Tick()
+    {
+        if (!isTicking) return;
+        if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            isTicking = false;
+        }
+
+        remainingTime -= Time.deltaTime;
     }
 }
